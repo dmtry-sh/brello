67e98812693411862bd01c98c24eba6e40da79e8
Dmitry Shuklinov <shuklinov.d@yandex.ru>
2024-11-27 12:25:45 +0300

    feat: formatting ci added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState
-
-} from "react";
-import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 
 import "./App.css";
-
-
-
-
+import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
@@ -16,11 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <a
-
-
-
-          href="https://vite.dev"         target="_blank">
+        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -36,7 +26,7 @@ function App() {
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
 export default App;
