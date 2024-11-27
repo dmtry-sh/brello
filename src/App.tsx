dfc171f36e487f69db14f5a4b65f435069a5d40b
Dmitry Shuklinov <shuklinov.d@yandex.ru>
2024-11-27 12:23:24 +0300

    debug: check CI for failure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useState
+
+} from "react";
+import reactLogo from "./assets/react.svg";
 
 import "./App.css";
-import reactLogo from "./assets/react.svg";
+
+
+
+
 import viteLogo from "/vite.svg";
 
 function App() {
@@ -10,7 +16,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a
+
+
+
+          href="https://vite.dev"         target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -26,7 +36,7 @@ function App() {
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  );
+  )
 }
 
 export default App;
